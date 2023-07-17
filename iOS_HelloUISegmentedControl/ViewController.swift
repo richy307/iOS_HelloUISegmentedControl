bd59de611d0e15961ef7d64d9e8f887580cce2f7
Richy <richy@webcomm.com.tw>
2023-07-17 21:09:29 +0800

    connect Action, sender.selectedSegmentIndex 目前選項

--- iOS_HelloUISegmentedControl/ViewController.swift
+++ iOS_HelloUISegmentedControl/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func myToggleChanged(_ sender: UISegmentedControl) {
+        // selectedSegmentIndex：目前選到的選項
+        if sender.selectedSegmentIndex == 0 {
+            print("Public")
+        } else {
+            print("Private")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
