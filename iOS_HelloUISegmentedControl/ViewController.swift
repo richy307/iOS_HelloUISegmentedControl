d0ae56288539dab4853b9acf000be91f4206ae5b
Richy <richy@webcomm.com.tw>
2023-07-17 21:23:04 +0800

    new 5 options UISegmentedControl

--- iOS_HelloUISegmentedControl/ViewController.swift
+++ iOS_HelloUISegmentedControl/ViewController.swift
@@ -19,6 +19,22 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func myToggleChanged_5(_ sender: UISegmentedControl) {
+            
+        if sender.selectedSegmentIndex == 0 {
+            view.backgroundColor = .white
+        } else if sender.selectedSegmentIndex == 1 {
+            view.backgroundColor = .black
+        } else if sender.selectedSegmentIndex == 2 {
+            view.backgroundColor = .red
+        } else if sender.selectedSegmentIndex == 3 {
+            view.backgroundColor = .orange
+        } else if sender.selectedSegmentIndex == 4 {
+            view.backgroundColor = .yellow
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
